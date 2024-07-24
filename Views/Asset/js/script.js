92f699a38196d1d53bb6a21dd40aef58cf1e82d7
MavesDev <littleudel@gmail.com>
2024-07-24 20:34:23 +0700

    Update : Script.js & Design Halaman

--- Views/Asset/js/script.js
+++ Views/Asset/js/script.js
@@ -1,50 +1,64 @@
-const loginUrl = "http://localhost:8080/api/v1/users/login";
+const loginUrl = "http://localhost:8080/api/v1/users/register";
 
-const loginData = {
-  username: "anis",
-  password: "admin",
-};
+const data = "./data.json";
 
-// uhhh coba simplify
-// async function loginUser(data) {
-//   try {
-//     const response = await fetch(loginUrl, {
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+async function regisUser(data) {
+  fetch(loginUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(JSON.stringify(data));
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+async function main() {
+  const data = await fetchData("./data.json");
+  await regisUser(data);
+}
+
+// Pertama, muat data dari file data.json
+// fetch("./data.json")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // Kirimkan data tersebut ke endpoint API
+//     return fetch(loginUrl, {
 //       method: "POST",
 //       headers: {
 //         "Content-Type": "application/json",
 //       },
 //       body: JSON.stringify(data),
 //     });
-
+//   })
+//   .then((response) => {
 //     if (!response.ok) {
-//       throw new Error("Network response was not ok " + response.statusText);
+//       console.log(response);
+//       throw new Error("Network response was not ok");
 //     }
-
-//     const responseData = await response.json();
-//     console.log("Login Response:", responseData);
-//   } catch (error) {
-//     console.error("There has been a problem with your fetch operation:", error);
-//   }
-// }
-
-// loginUser(loginData);
-
-
-async function loginUser(data) {
-  fetch(loginUrl,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }
-  )
-    .then(response => response.json())
-    .then(data => {
-      console.log(JSON.stringify(data));
-    })
-    .catch(error => console.error('Error fetching data:', error));
-}
-
-loginUser(loginData);
+//     response.json();
+//   })
+//   .then((data) => console.log("Success:", data))
+//   .catch((error) => console.error("Error:", error));
